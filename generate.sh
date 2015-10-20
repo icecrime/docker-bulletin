64e5bc0ce0b7bd57f081129821db2ace6b0e39ad
Arnaud Porterie <arnaud.porterie@docker.com>
2015-10-19 22:41:06 -0700

    Commit generated reports
    Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

--- generate.sh
+++ generate.sh
@@ -6,6 +6,8 @@ generate () {
 
 for dir in *; do
     if [ -d $dir ]; then
-        (cd $dir && generate $1)
+        (cd $dir && generate $1) > $dir/$(date "+%Y-%m-%d").md
     fi
 done
+
+git add * && git commit -sm "Generate reports" && git push origin
